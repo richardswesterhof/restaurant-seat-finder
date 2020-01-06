cf181bdd049ba249274a7e7577f54fd34f64c4d8
richardswesterhof <richardswesterhof@gmail.com>
2020-01-06 19:25:02 +0100

    changed name of api token environment variables

--- src/top_secret_files/tokenList.js
+++ src/top_secret_files/tokenList.js
@@ -1,4 +1,4 @@
 export default {
-  mapboxToken: process.env.NODE_ENV === 'development' ? 'EXAMPLE_DEVELOPMENT_KEY' : process.env.DEPLOYMENT_MAPBOX_KEY,
-  locationIqToken: process.env.NODE_ENV === 'development' ? 'EXAMPLE_DEVELOPMENT_KEY' : process.env.DEPLOYMENT_LOCATIONIQ_KEY,
+  mapboxToken: process.env.NODE_ENV === 'development' ? 'EXAMPLE_DEVELOPMENT_KEY' : process.env.VUE_APP_MAPBOX_KEY,
+  locationIqToken: process.env.NODE_ENV === 'development' ? 'EXAMPLE_DEVELOPMENT_KEY' : process.env.VUE_APP_LOCATIONIQ_KEY,
 }
